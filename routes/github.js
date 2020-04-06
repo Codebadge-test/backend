efe69944d5e8e7a0a58af3a920cf09cb844556ef
ayushnagar123 <an431999@gmail.com>
2020-04-06 07:19:11 +0530

    correction for hosting backend for cookies

--- routes/github.js
+++ routes/github.js
@@ -6,10 +6,14 @@ const bodyParser = require('body-parser')
 const request = require('request')
 
 if(process.env.NODE_ENV=="production"){
-var frontend = "https://codebadge-frontend.netlify.com"
+    var frontend = "https://codebadge-frontend.netlify.com"
+    var clientId = "9a3d93461f11673e4164"
+    var clientSecret = "3c799896323f37a83ebfc0acf208f611540998b4"
 }
 else{
     var frontend = "http://localhost:3000/"
+    var clientId = "90483cf9b8d2f46424fa"
+    var clientSecret = "52d0a00f6b20ec70425bef9126c2532b382b4bc3"
 }
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
@@ -26,8 +30,8 @@ router.get('/auth',(req,res)=>{
     }
     request.post("https://github.com/login/oauth/access_token/",
     {
-        client_id:"9a3d93461f11673e4164",
-        client_secret:"3c799896323f37a83ebfc0acf208f611540998b4",
+        client_id:clientId,
+        client_secret:clientSecret,
         code:code
     },
     (result)=>{
