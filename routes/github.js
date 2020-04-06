b8c79532b964aeb97b13d9360fdb30bb146112ab
ayushnagar123 <an431999@gmail.com>
2020-04-07 01:06:44 +0530

    cookies error resolve

--- routes/github.js
+++ routes/github.js
@@ -4,6 +4,8 @@ var cookieParser = require('cookie-parser')
 const Trello = require('trello')
 const bodyParser = require('body-parser')
 const request = require('request')
+const axios = require("axios")
+var querystring = require('querystring');
 if(process.env.NODE_ENV=="production"){
     var frontend = "https://codebadge-frontend.netlify.com"
     var clientId = "9a3d93461f11673e4164"
@@ -19,6 +21,7 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.use(cookieParser())
 router.get('/auth',(req,res)=>{
     var {query} = req;
+    console.log(req)
     var {code} = query;
     console.log(code)
     if(!code){
@@ -27,20 +30,57 @@ router.get('/auth',(req,res)=>{
             message:"Error: no code"
         })
     }
-    request.post("https://github.com/login/oauth/access_token/",
+    axios.post("https://github.com/login/oauth/access_token/",
     {
         client_id:clientId,
         client_secret:clientSecret,
         code:code
-    },
-    (result)=>{
-        console.log("hit");
-        const data= res.data;
-        console.log(code)
-        res.cookie('token',code,{ maxAge: 1000 * 60 * 10, httpOnly: false })
-        res.redirect(frontend)
     })
-})
+    .then((result)=>{
+        const data= result.data;
+        var token = querystring.parse(data).access_token
+        res.header('Access-Control-Allow-Origin','*')
+        res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,COntent-Type,Accept');
+        res.header("Access-Control-Allow-Credentials",true)
+        res.cookie('token',token,{ maxAge: 1000 * 60 * 10, httpOnly: false })
+        res.redirect(frontend+`user`)
+    })
+        .catch(err => {
+            console.log(err)
+            res.send(err)
+        });
+     })
+    
+     router.post('/user',(req,res)=>{
+        var token = req.body.token;
+        var userData = {
+            username:null,
+            orgs:[],
+            avatar:null
+        }
+        console.log("hiting github/user")
+        axios.get("https://api.github.com/user",header={Authorization: `token ${token}`})
+            .then(resp=>{
+                var data = resp.data
+                userData.username= data.login
+                userData.avatar = data.avatar
+                if(!userData.username){
+                    return data;
+                }
+            })
+            .then(resp1=>{
+                axios.get(resp.orgs,header={Authorization: `token ${token}`})
+                    .then(resp=>{
+                        var data = resp.data
+                        userData.orgs= data
+                        if(!userData.username){
+                            res.json(userData);
+                        }
+                    })
+            })
+            .catch(err=>res.send(err))
+    })
+
 
 router.get('/install',(req,res)=>{
     res.redirect(frontend+'/register')
